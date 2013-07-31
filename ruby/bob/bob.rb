21488ef4a431b01888257b6718df44f67c2017b2
Oriol Gual <oriol.gual@gmail.com>
2013-07-31 18:33:41 +0200

    Make Bob dumb

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,15 +1,29 @@
 class Bob
   def hey(text)
-    message = Message.new(text)
-
-    return 'Fine. Be that way!' if message.empty?
-    return 'Woah, chill out!'   if message.shouting?
-    return 'Sure.'              if message.question?
-
-    'Whatever.'
+    TeenagerAnswer.new(text).answer
   end
 
   private
+  class TeenagerAnswer
+    attr_reader :text
+
+    def initialize(text)
+      @text = text.to_s
+    end
+
+    def answer
+      return 'Fine. Be that way!' if message.empty?
+      return 'Woah, chill out!'   if message.shouting?
+      return 'Sure.'              if message.question?
+
+      'Whatever.'
+    end
+
+    def message
+      @message ||= Message.new(text)
+    end
+  end
+
   class Message
     attr_reader :text
 
